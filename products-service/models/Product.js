58fd23fe61c5e2780d5c4ba82a4624136e7f3377
carlosandresdiazs <caandisa@gmail.com>
2026-09-08 17:31:03 -0500

    "Update Product.js products-service\models"

--- products-service/models/Product.js
+++ products-service/models/Product.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
- name: String,
- price: Number,
- category: String,
+    name: String,
+    price: Number,
+    category: String,
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
