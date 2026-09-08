9c4dd29d06d37e416a3152bdc0a1a2c10817be51
carlosandresdiazs <caandisa@gmail.com>
2026-09-08 17:33:40 -0500

    "Update server.js orders-service"

--- orders-service/server.js
+++ orders-service/server.js
@@ -8,14 +8,14 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost/orders-db", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.post("/orders", async (req, res) => {
- const order = new Order(req.body);
- await order.save();
- res.send({ message: "Pedido realizado", order });
+  const order = new Order(req.body);
+  await order.save();
+  res.send({ message: "Pedido realizado", order });
 });
 
 app.get("/orders", async (req, res) => {
- const orders = await Order.find();
- res.send(orders);
+  const orders = await Order.find();
+  res.send(orders);
 });
 
-app.listen(4003, () => console.log("📦 Servicio de Pedidos en http://localhost:4003"));
+app.listen(4003, () => console.log("Servicio de Pedidos en http://localhost:4003"));
